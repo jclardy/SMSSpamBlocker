597d9663fd100c77ae68ade0648a2dcf9466117c
Jason Clardy <jason@teltech.co>
2019-04-05 16:37:16 -0400

    Adding files

---
+++ SMSFilter/MessageFilterExtension.swift
@@ -0,0 +1,70 @@
+//
+//  MessageFilterExtension.swift
+//  SMSFilter
+//
+//  Created by Jason Clardy on 3/20/19.
+//  Copyright © 2019 TelTech Systems Inc. All rights reserved.
+//
+
+import IdentityLookup
+import NaturalLanguage
+
+final class MessageFilterExtension: ILMessageFilterExtension {}
+
+extension MessageFilterExtension: ILMessageFilterQueryHandling {
+    
+    func handle(_ queryRequest: ILMessageFilterQueryRequest, context: ILMessageFilterExtensionContext, completion: @escaping (ILMessageFilterQueryResponse) -> Void) {
+        // First, check whether to filter using offline data (if possible).
+        let offlineAction = self.offlineAction(for: queryRequest)
+        
+        switch offlineAction {
+        case .allow, .filter:
+            // Based on offline data, we know this message should either be Allowed or Filtered. Send response immediately.
+            let response = ILMessageFilterQueryResponse()
+            response.action = offlineAction
+            
+            completion(response)
+            
+        case .none:
+            // Based on offline data, we do not know whether this message should be Allowed or Filtered. Defer to network.
+            // Note: Deferring requests to network requires the extension target's Info.plist to contain a key with a URL to use. See documentation for details.
+            context.deferQueryRequestToNetwork() { (networkResponse, error) in
+                let response = ILMessageFilterQueryResponse()
+                response.action = .none
+                
+                if let networkResponse = networkResponse {
+                    // If we received a network response, parse it to determine an action to return in our response.
+                    response.action = self.action(for: networkResponse)
+                } else {
+                    NSLog("Error deferring query request to network: \(String(describing: error))")
+                }
+                
+                completion(response)
+            }
+        }
+    }
+    
+    private func offlineAction(for queryRequest: ILMessageFilterQueryRequest) -> ILMessageFilterAction {
+        // Replace with logic to perform offline check whether to filter first (if possible).
+        guard let sentimentPredictor = try? NLModel(mlModel: SpamClassifier().model),
+            let body = queryRequest.messageBody else {
+                return .none
+        }
+        
+        let label = sentimentPredictor.predictedLabel(for: body)
+        switch label {
+        case "spam":
+            return .filter
+        case "safe":
+            return .allow
+        default:
+            return .none
+        }
+    }
+    
+    private func action(for networkResponse: ILNetworkResponse) -> ILMessageFilterAction {
+        // Replace with logic to parse the HTTP response and data payload of `networkResponse` to return an action.
+        return .none
+    }
+    
+}
